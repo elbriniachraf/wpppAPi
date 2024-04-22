14ce8a0183032df8edcf9f2460b2f03139b8dda0
elbriniachraf <elbriniachraf@gmail.com>
2024-04-22 13:06:10 +0200

    first commit

--- FrontEnd/src/Component/Header/Header.jsx
+++ FrontEnd/src/Component/Header/Header.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function Header() {
     return (
         <div className='bg-purple-600  px-14 py-4'>
-            <img src="/assets/images/LOGOBLANC.png" alt="logo_mahalat" />
         </div>
     );
 }
